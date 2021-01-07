d6c23262ffb4952fabc1ba9f4411e27e967be275
Hkc-1673 <habibkc71@gmail.com>
2021-01-08 01:04:42 +0200

    Added projects component

---
+++ client/src/components/Projects.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Projects.css"
+
+function Projects() {
+    return (
+        <div className="projects">
+            <div className="projects__header">
+                <h2>Projelerim</h2>
+                <p>Şimdiye kadar bir çok proje hazırladım.</p>
+                <p>Çalışmalarımın bir kısmı aşağıda yer almaktadır.</p>
+            </div>
+            <div className="projects__container">
+                <div>
+                    <img src="" alt="" />
+                    <p>Proje linki</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>Proje linki</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>Proje linki</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Projects
