774ead201e2fa8c717426f6cfc9199d5d1368fe1
Hkc-1673 <habibkc71@gmail.com>
2021-01-08 01:31:48 +0200

    Added articles file

---
+++ client/src/components/Articles.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Articles.css"
+
+function Articles() {
+    return (
+        <div className="articles">
+            <h2>Makalelerim</h2>
+            <div className="articles__container">
+                <div>Makalelerim</div>
+                <div>Makalelerim</div>
+                <div>Makalelerim</div>
+            </div>
+        </div>
+    )
+}
+
+export default Articles
